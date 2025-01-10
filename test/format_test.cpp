7099379d03bc0176950f60bc629c84506b0f7102
LIN <13968527371@163.com>
2025-01-10 10:58:34 +0800

    test: add googletest for format_test

--- test/format_test.cpp
+++ test/format_test.cpp
@@ -1,13 +1,9 @@
+#include "gtest/gtest.h"
+
 #include "format.h"
 
-int main(int argc, char const *argv[]) {
-  /* code */
-
-  syfmt::print("hello {}\n", "world");
-
-  syfmt::print("{}{}{}{}{}\n", 'h', 'e', 'l', 'l', 'o');
-
-  syfmt::print("{} {} {} {} {}\n", 'a', 123, (float)1.23, (double)1.23, "c-style string");
-
-  return 0;
+TEST(syfmt, syfmt) {
+  EXPECT_NO_THROW(syfmt::print("hello {}\n", "world"));
+  EXPECT_NO_THROW(syfmt::print("{}{}{}{}{}\n", 'h', 'e', 'l', 'l', 'o'));
+  EXPECT_NO_THROW(syfmt::print("{} {} {} {} {}\n", 'a', 123, (float)1.23, (double)1.23, "c-style string"));
 }
